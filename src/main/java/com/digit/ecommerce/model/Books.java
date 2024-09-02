5f9f267fef86eaf9ceb88d90f7b16b87013f281d
Sri Danthuluru <Sri.Danthuluru@godigit.com>
2024-09-02 19:19:09 +0530

    hi

--- src/main/java/com/digit/ecommerce/model/Books.java
+++ src/main/java/com/digit/ecommerce/model/Books.java
@@ -1,6 +1,7 @@
 package com.digit.ecommerce.model;
 
 import com.digit.ecommerce.dto.BooksDto;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -9,12 +10,14 @@ import lombok.NoArgsConstructor;
 import org.hibernate.engine.internal.Cascade;
 
 import java.util.List;
+import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
 @Table(name = "Books_data")
+@ToString(exclude = {"wishlists", "orders"})
 public class Books {
 
     @Id
@@ -37,9 +40,17 @@ public class Books {
     @Column(name = "book_quantity")
     private Long bookQuantity;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "image_id" )
+    @OneToMany(mappedBy = "book", cascade = CascadeType.PERSIST)
+    @JsonIgnore
+    private List<Wishlist> wishlists;
+
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "image_id")
     private AddImage addImage;
+
+    @OneToMany(mappedBy = "book", cascade = CascadeType.PERSIST)
+    @JsonIgnore
+    private List<Orders> orders;
 
     public Books(BooksDto booksDto) {
         this.bookName = booksDto.getBookName();
@@ -50,3 +61,58 @@ public class Books {
         this.addImage = booksDto.getAddImage(); // Assuming BooksDto has a field for AddImage
     }
 }
+
+
+//
+//package com.digit.ecommerce.model;
+//
+//import com.digit.ecommerce.dto.BooksDto;
+//import com.fasterxml.jackson.annotation.JsonManagedReference;
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import org.hibernate.engine.internal.Cascade;
+//
+//import java.util.List;
+//
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Data
+//@Entity
+//@Table(name = "Books_data")
+//public class Books {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "Book_id")
+//    private Long id;
+//
+//    @Column(name = "Book_name")
+//    private String bookName;
+//
+//    @Column(name = "Book_author")
+//    private String bookAuthor;
+//
+//    @Column(name = "Book_description")
+//    private String bookDescription;
+//
+//    @Column(name = "book_price")
+//    private Long bookPrice;
+//
+//    @Column(name = "book_quantity")
+//    private Long bookQuantity;
+//
+//    @OneToOne(cascade = CascadeType.ALL)
+//    @JoinColumn(name = "image_id" )
+//    private AddImage addImage;
+//
+//    public Books(BooksDto booksDto) {
+//        this.bookName = booksDto.getBookName();
+//        this.bookAuthor = booksDto.getBookAuthor();
+//        this.bookDescription = booksDto.getBookDescription();
+//        this.bookPrice = booksDto.getBookPrice();
+//        this.bookQuantity = booksDto.getBookQuantity();
+//        this.addImage = booksDto.getAddImage();
+//    }
+//}
--- src/main/java/com/digit/ecommerce/model/Orders.java
+++ src/main/java/com/digit/ecommerce/model/Orders.java
@@ -1,6 +1,7 @@
 package com.digit.ecommerce.model;
 
 import com.digit.ecommerce.dto.OrderDTO;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,10 +10,13 @@ import lombok.NoArgsConstructor;
 import jakarta.persistence.*;
 import java.time.LocalDate;
 
+import lombok.ToString;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@ToString(exclude = {"user", "book"})
 public class Orders {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,26 +27,66 @@ public class Orders {
     private String address;
     private boolean cancel;
     private String status;
-    private String shippingStatus; // New column
+    private String shippingStatus;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
-    @JsonIgnore
+    @JsonBackReference
     private User user;
 
     @ManyToOne
     @JoinColumn(name = "book_id")
+    @JsonBackReference
     private Books book;
-
-    public Orders(OrderDTO orderDTO, User user, Books book) {
-        this.orderDate = LocalDate.now();
-        this.price = book.getBookPrice();
-        this.quantity = orderDTO.getQty();
-        this.address = orderDTO.getAddress();
-        this.cancel = false;
-        this.status = "ordered";
-        this.shippingStatus = "placed"; // Set initial shipping status
-        this.user = user;
-        this.book = book;
-    }
 }
+
+//
+//
+////package com.digit.ecommerce.model;
+////
+////import com.digit.ecommerce.dto.OrderDTO;
+////import com.fasterxml.jackson.annotation.JsonIgnore;
+////import lombok.AllArgsConstructor;
+////import lombok.Data;
+////import lombok.NoArgsConstructor;
+////
+////import jakarta.persistence.*;
+////import java.time.LocalDate;
+////
+////@Data
+////@AllArgsConstructor
+////@NoArgsConstructor
+////@Entity
+////public class Orders {
+////    @Id
+////    @GeneratedValue(strategy = GenerationType.IDENTITY)
+////    private Long id;
+////    private LocalDate orderDate;
+////    private Long price;
+////    private Long quantity;
+////    private String address;
+////    private boolean cancel;
+////    private String status;
+////    private String shippingStatus; // New column
+////
+////    @ManyToOne
+////    @JoinColumn(name = "user_id")
+////    @JsonIgnore
+////    private User user;
+////
+////    @ManyToOne
+////    @JoinColumn(name = "book_id")
+////    private Books book;
+////
+////    public Orders(OrderDTO orderDTO, User user, Books book) {
+////        this.orderDate = LocalDate.now();
+////        this.price = book.getBookPrice();
+////        this.quantity = orderDTO.getQty();
+////        this.address = orderDTO.getAddress();
+////        this.cancel = false;
+////        this.status = "ordered";
+////        this.shippingStatus = "placed"; // Set initial shipping status
+////        this.user = user;
+////        this.book = book;
+////    }
+////}
